c625b2c5bba4d815123d374d06c555cb3c63692e
unifyintel <67732883+unifyintel@users.noreply.github.com>
2020-07-02 01:31:23 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -14,10 +14,7 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-get "/contacts" do
-  @contacts = Contact.all
-  erb :index
-end
+
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
